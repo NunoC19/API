55a480ba319eee4410e1ab2951384359e54a2b33
nunocoelho <nuno.coelho2022@gmail.com>
2022-02-23 12:28:16 -0100

    up

---
+++ routes/getDataRoute.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const res = require('express/lib/response')
+const router = express.Router()
+
+const exemploModel = require('../models/exemploModel')
+
+router.get('/:idArduino', (req, res) => {
+    exemploModel.findOne({'idArduino':{$eq: req.params.idArduino}})
+    .exec()
+    .then((result)=>{
+        if(result != 0){
+            return res.json({
+                temp:result.temp,
+                hum: result.hum
+            })
+        }
+        return res.json({
+            msg:'não encontrado'
+        })
+        
+    })
+    .catch(error =>{
+        console.log(error)
+    })
+})
+
+module.exports = router
+
+
